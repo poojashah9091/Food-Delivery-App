64bfad5050ab65fbb883a48f5a5fe4cb749ebe23
Pooja Shah <poojashah9091@gmail.com>
2023-04-12 18:07:16 -0400

    Header component with routing configured for homepage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,27 +5,42 @@ import DiscountOutlinedIcon from '@mui/icons-material/DiscountOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import "./style.scss";
+import { useDispatch } from "react-redux";
+import { filterRestaurants } from "../../data/restaurantListSlice";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () =>{
+
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    const handleSearch = (e) =>{
+        dispatch(filterRestaurants(e.target.value));
+    }
+
     return(
         <div className="header_container">
-            <div>
-                <img src={Logo} alt="Logo" width={50} height={50}/>
+            <div className="brand">
+                <Link to="/" className="brand_logo"> <img src={Logo} alt="Logo"/></Link>
+                <Link to="/" className="brand_name">  <label>Food Mart</label></Link>
             </div>
             <div>
                 <ul className="header_menu">
-                    <li>
-                        <SearchIcon/>
-                        <input type="text" name="Search" className="searchbox"/>
-                    </li>
+                    {
+                        location.pathname==="/" &&
+                        <li>
+                            <SearchIcon/>
+                            <input type="text" name="Search" className="searchbox" onChange={handleSearch} placeholder="Search Restaurant"/>
+                        </li>
+                    }
                     <li>
                         <DiscountOutlinedIcon/>
                         Offers
                     </li>
-                    <li>
+                    {/* <li>
                         <PersonOutlineOutlinedIcon/>
                         Sign In
-                    </li>
+                    </li> */}
                     <li>
                         <ShoppingCartOutlinedIcon/>
                         Cart
