9422957e85b1fc2eb3bfac5a30990d1ece36b2b2
Pooja Shah <poojashah9091@gmail.com>
2023-04-16 14:56:43 -0400

    Restaurant Tile configuration to view restaurant details

--- src/components/RestaurantTile/index.js
+++ src/components/RestaurantTile/index.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import StarRateRoundedIcon from '@mui/icons-material/StarRateRounded';
+import DiscountRoundedIcon from '@mui/icons-material/DiscountRounded';
+import "./style.scss";
+import Badge from "../../customComponents/Badge.js";
+import { Link } from "react-router-dom";
 
-const RestaurantTile = ({imageSrc, restaurantName, cuisines, avgRating, deliveryTime, costForTwoString, discountScheme}) =>{
+const RestaurantTile = ({ restaurantId, badge, imageSrc, restaurantName, cuisines, avgRating, deliveryTime, costForTwoString, discountScheme}) =>{
+
+    const [showRestaurant, setShowRestaurant] = useState(false);
+    const handleRestaurantTileClick = () =>{
+        setShowRestaurant(true);
+    }
+
     return(
-        <div className="restaurantTileContainer">
-            <img src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${imageSrc}`} 
-                alt="restaurantImg"/>
-            <label>{restaurantName}</label>
-            <label>{cuisines}</label>
-            <div>
-                <div>{avgRating}</div>
-                <div>{deliveryTime}</div>
-                <div>{costForTwoString}</div>
+        <>
+        <Link to={`/restaurant/${restaurantId}`}>
+            <div className="restaurant_tile_container" id={restaurantId} onClick={handleRestaurantTileClick}>
+                {badge && 
+                    <Badge text="PROMOTED"/>
+                }
+                <img src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${imageSrc}`} 
+                    alt={restaurantName}
+                />
+                <label className="restaurant_name">{restaurantName}</label>
+                <label className="cuisines">{cuisines}</label>
+                <div className="meta_info">
+                    {avgRating && 
+                        <div className={avgRating>=4.0 ? "rating high_rating" : "rating avg_rating"}>
+                            <StarRateRoundedIcon/>
+                            <label>{avgRating}</label>
+                        </div>
+                    }
+                    <div>{deliveryTime}</div>
+                    <div>{costForTwoString}</div>
+                </div>
+                {discountScheme && 
+                    <div className="offer">
+                        <DiscountRoundedIcon/>
+                        <label>{discountScheme}</label>
+                    </div>
+                }
             </div>
-            <label>{discountScheme}</label>
-        </div>
+            </Link>
+            {/* {
+              showRestaurant && <Link to={`/restaurants/${restaurantId}`}/>
+            } */}
+        </>
     )
 }
 
