d8ff474b5d657610e8036d99f308d26b9081c01a
Pooja Shah <poojashah9091@gmail.com>
2023-04-17 17:10:53 -0400

    commented some unused code in store component

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import restaurantDataSliceReducer from "../data/restaurantDataSlice";
+// import restaurantDataSliceReducer from "../data/restaurantDataSlice";
 import allRestaurantsSliceReducer from "../data/restaurantListSlice";
 
 const store = configureStore({
     reducer:{
         allRestaurants: allRestaurantsSliceReducer,
-        restaurantData: restaurantDataSliceReducer
+        // restaurantData: restaurantDataSliceReducer
     }
 });
 
